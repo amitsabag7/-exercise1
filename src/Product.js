2c5047d0fcb21e9757db929324fc9cda1a4ce836
ofriaker <ofriaker@gmail.com>
2022-11-19 17:19:58 +0200

    Changes to style and css

--- src/Product.js
+++ src/Product.js
@@ -7,11 +7,11 @@ export default class Product extends Component {
   return (
     <div className='product-item'>        
         <div className='product-details'>
-          <p>Product name:{this.props.name}</p>
-          <p>price:{this.props.price}</p>
+          <p>Product name: {this.props.name}</p>
+          <p>Price: {this.props.price}</p>
         </div>
-      <div className='product-image'>
-        <img src={this.props.productImageUrl} alt="" />        
+      <div>
+        <img  className='product-image' src={this.props.productImageUrl} alt="" />        
       </div>
     </div>
   );
