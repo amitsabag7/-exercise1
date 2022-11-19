e7f662ebf1d01a375ed809e35094c50168fe9e48
root <root@Amit-Sabag.localdomain>
2022-11-19 16:06:43 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { products } from './data';
-import ProductList from './ProductList';
+import { products } from './data.js';
+import ProductList from './ProductList.js';
 
 export default function App() {
     return (
